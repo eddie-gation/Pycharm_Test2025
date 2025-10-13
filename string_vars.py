401394016353a8836980b2c378c90be4f1bdbb8f
seonghyuek kim <johnkim9597@gmail.com>
2025-10-13 14:13:08 +0200

    playing further with git

--- string_vars.py
+++ string_vars.py
@@ -1,1 +1,2 @@
 print("now its going on githu222222b")
+print("where is everything")
--- var.py
+++ var.py
@@ -1,5 +1,1 @@
-print("ahhhㅓㅣ어리ㅏh")
-print("now its going on github")
-
-print("now its going on github")
-print("ABDC")
+print("ahhhh")
