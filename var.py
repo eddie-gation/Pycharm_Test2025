0dcd386f1d3a92d421bcc214f1f95eb3ce392016
seonghyuek kim <johnkim9597@gmail.com>
2025-10-13 14:48:43 +0200

    number formats and limitations

--- var.py
+++ var.py
@@ -1,1 +1,39 @@
 print("ahhhh")
+
+if __name__ == "__main__":   #
+    print("hello")
+    print("world")
+
+
+    a =1.79e308
+    b = 10*a
+    print('a = {}, b = {}'.format(a, b))
+
+    c = 4.94e-324  #loweset number
+    d= c/10
+    print('c={}, d={}'.format(c, d))
+
+
+    print("-------------------")
+
+    a1  = 454.757676
+    print("a1 = {}".format(a1))
+    print(type(a1))
+    a2 = round(a1,2)
+    print("a2 = {}".format(a2))
+    print(type(a2))
+    a3 = int(a1)
+    print("a3 = {}".format(a3))
+    print(type(a3))
+
+
+    a4 ='4545.868'
+    print("a4 = {}".format(a4))
+    print(type(a4))
+
+    a5 = float(a4)
+    print("a5 = {}".format(a5))
+    print(type(a5))
+
+    b1 = 0b100101
+    print("b1 = {}".format(b1))
