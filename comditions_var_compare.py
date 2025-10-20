46448e7795ab78f015ae55f3ef75effae407fb7a
seonghyuek kim <johnkim9597@gmail.com>
2025-10-20 12:57:34 +0200

    conditions: match, assignment

---
+++ comditions_var_compare.py
@@ -0,0 +1,33 @@
+a = 10
+b = 20
+
+if a>b:
+    print('a >= b')
+else:
+    print('a < b')
+
+
+s1 = 'abc'
+s2 = 'abcd'
+
+if s1 == s2:
+    print('s1 == s2')
+else:
+    print('s1 != s2')
+
+
+a = 10 if s1 ==s2 else 20
+print(a)
+
+
+s1 =  ('abc')
+
+match s1:
+    case 'a':
+        print('s1 = a')
+    case 'b':
+        print('s1 =abc')
+    case 'abcd':
+        print('s1 =abcd')
+    case _:
+        print('s1 not matched')
